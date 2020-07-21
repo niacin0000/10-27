3782a3597f87e5df3bab8fc5ba4f129109d5f1e7
jckim1102 <jckim1102@naver.com>
2020-07-21 17:08:50 +0900

    servertest
    servertest

--- Assets/Script/RoomData.cs
+++ Assets/Script/RoomData.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon;
+using Photon.Pun;
 
-public class RoomData : MonoBehaviour {
+public class RoomData :  MonoBehaviourPun
+{
 
     public string roomName = "";
     public int playerCount = 0;
