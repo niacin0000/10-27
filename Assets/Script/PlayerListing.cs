a8828de53522286e6ef65611c87cf5cc9c317413
passerby10 <66811453+passerby10@users.noreply.github.com>
2020-08-03 18:24:23 +0900

    playerlist.ver2

--- Assets/Script/PlayerListing.cs
+++ Assets/Script/PlayerListing.cs
@@ -12,10 +12,12 @@ public class PlayerListing : MonoBehaviour
 
     public Player Player { get; private set; }
 
-    public void SetPlayerInfo(Player player)
+    public bool TeamRedBlu;
+
+    public void SetPlayerInfo(Player player, bool RedBlu)
     {
         Player = player;
         _text.text = player.NickName;
-
+        TeamRedBlu = RedBlu;
     }
 }
--- Assets/Script/PlayerListingsMenu.cs
+++ Assets/Script/PlayerListingsMenu.cs
@@ -3,19 +3,34 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerListingsMenu : MonoBehaviourPunCallbacks
 {
     [SerializeField]
-    private Transform _content;
+    private Transform _content_Red;
+    [SerializeField]
+    private Transform _content_Blu;
     [SerializeField]
     private PlayerListing _playerListing;
+    [SerializeField]
+    private Button _RedTeamButton;
+    [SerializeField]
+    private Button _BluTeamButton;
 
     private List<PlayerListing> _listing = new List<PlayerListing>();
+    private List<PlayerListing> _listing_Red = new List<PlayerListing>();
+    private List<PlayerListing> _listing_Blu = new List<PlayerListing>();
+
+    private bool RedOrBlu;
 
     private void Awake()
     {
         GetCurrentRoomPlayers();
+    }
+
+    private void Start()
+    {
     }
 
     private void GetCurrentRoomPlayers()
@@ -29,13 +44,45 @@ public class PlayerListingsMenu : MonoBehaviourPunCallbacks
     
     private void AddPlayerListing(Player player)
     {
-        PlayerListing listing = Instantiate(_playerListing, _content);
+        // team
+        //if (_listing_Blu.Count >= _listing_Red.Count)
+        //{
+        //    PlayerListing listing = Instantiate(_playerListing, _content_Red);
+        //    if (listing != null)
+        //    {
+        //        listing.SetPlayerInfo(player, true);
+        //        _listing_Red.Add(listing);
+        //    }
+        //}
+        //else
+        //{
+        //    PlayerListing listing = Instantiate(_playerListing, _content_Blu);
+        //    if (listing != null)
+        //    {
+        //        listing.SetPlayerInfo(player, false);
+        //        _listing_Blu.Add(listing);
+        //    }
+        //}
+        // solo
+        PlayerListing listing = Instantiate(_playerListing, _content_Red);
         if (listing != null)
         {
-            listing.SetPlayerInfo(player);
+            listing.SetPlayerInfo(player, true);
             _listing.Add(listing);
         }
     }
+
+    //public void ChangeTeamBlu(Player player)
+    //{
+    //    PlayerListing listing_R = Instantiate(_playerListing, _content_Red);
+    //    PlayerListing listing_B = Instantiate(_playerListing, _content_Blu);
+    //    if (listing_B != null)
+    //    {
+    //        listing_B.SetPlayerInfo(player, true);
+    //        _listing_Blu.Remove(listing_R);
+    //        _listing_Red.Add(listing_B);
+    //    }
+    //}
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         AddPlayerListing(newPlayer);
@@ -44,7 +91,18 @@ public class PlayerListingsMenu : MonoBehaviourPunCallbacks
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
         int index = _listing.FindIndex(x => x.Player == otherPlayer);
-        if(index != -1)
+        //if (index_R != -1)
+        //{
+        //    Destroy(_listing_Red[index_R].gameObject);
+        //    _listing_Red.RemoveAt(index_R);
+        //}
+        //int index_B = _listing_Blu.FindIndex(x => x.Player == otherPlayer);
+        //if (index_B != -1)
+        //{
+        //    Destroy(_listing_Red[index_B].gameObject);
+        //    _listing_Red.RemoveAt(index_B);
+        //}
+        if (index != -1)
         {
             Destroy(_listing[index].gameObject);
             _listing.RemoveAt(index);
