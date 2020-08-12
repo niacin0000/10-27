58741c2acabfdd333d8fc2885a43b514ce1dba32
passerby10 <66811453+passerby10@users.noreply.github.com>
2020-08-12 18:44:00 +0900

    제발이것만은 살려주세요... 집에 처자식이 기다리고있어요...

--- Assets/Script/RoomManager.cs
+++ Assets/Script/RoomManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomManager : MonoBehaviourPunCallbacks
-{
+{ 
     public void LeaveRoom()
     {
         PhotonNetwork.LeaveRoom();
