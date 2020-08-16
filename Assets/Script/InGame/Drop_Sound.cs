aa5541db5198456d9ff098f32ee3870108ce1710
castlejm <mattcho@naver.com>
2020-08-16 19:00:32 +0900

    sound

--- Assets/Script/InGame/Drop_Sound.cs
+++ Assets/Script/InGame/Drop_Sound.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Drop_Sound : MonoBehaviour
 {
     private bool one = true;
+    public AudioClip[] audioClips;
+    AudioSource audiosource;
+    public AudioMixerGroup audioMixerGroup;
+
+    public void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.CompareTag("GROUND") && one)
         {
-            Debug.Log("Drop " + this.name);
+            if (this.name == "Drop_Es" || this.name == "Drop_Sw")
+            {
+                audiosource.clip = audioClips[0];
+                audiosource.outputAudioMixerGroup = audioMixerGroup;
+                audiosource.Play();
+                audiosource.loop = false;
+            }
+            else if (this.name == "Drop_Sh" || this.name == "Drop_St")
+            {
+                audiosource.clip = audioClips[1];
+                audiosource.outputAudioMixerGroup = audioMixerGroup;
+                audiosource.Play();
+                audiosource.loop = false;
+            }
             one = false;
         }
     }
